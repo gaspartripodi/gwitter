e762204f46dbe954e79dfb82cf66e3e9ad954038
gaspartripodi <gaspartdi@gmail.com>
2020-03-29 16:06:03 -0300

    Configuration adapted to TweetList in Home view

--- src/app/models/tweet.ts
+++ src/app/models/tweet.ts
@@ -2,11 +2,17 @@ export interface ITweet {
   created_at: Date;
   id: boolean;
   text: string;
+  truncated: string;
+  entities: {
+    urls: []
+  }
   user: {
     name: string;
     screen_name: string;
     verified: boolean;
     profile_image_url: string;
+    default_profile: boolean;
+    following: boolean;
   };
   retweet_count: number;
   favorite_count: number;
