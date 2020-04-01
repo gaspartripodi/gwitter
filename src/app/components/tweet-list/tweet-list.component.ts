82724543a964d49a423735f2d1be106cad6da553
gaspartripodi <gaspartdi@gmail.com>
2020-04-01 17:58:23 -0300

    TweetList Component changes
    Comments have been removed. Configured to disable spinner if a filter is enabled

--- src/app/components/tweet-list/tweet-list.component.ts
+++ src/app/components/tweet-list/tweet-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NgxSpinnerService } from "ngx-spinner";
 
 import { ITweet } from '../../models/tweet';
+import { ITweetListConfiguration } from 'src/app/models/tweetListConfiguration';
+
 import { ApiTwitterService } from '../../services/apitwitter/apitwitter.service';
 
 import { TWEET_LIMIT_PAGE, TWEET_CALLS_LIMIT } from '../../constants/constants';
@@ -16,31 +18,37 @@ export class TweetListComponent {
   tweets: ITweet[] = [];
   maxId: number;
   tweetCalls: number = 0;
+  tweetListConfiguration: ITweetListConfiguration;
 
   constructor(private apiTwitterService: ApiTwitterService, private spinner: NgxSpinnerService) {
-    // this.tweets = []; // to reset pagination (not necessary in this component)
-    // this.tweetCalls = 0; // to reset pagination (not necessary in this component)
     this.getTweets();
+    this.tweetListConfiguration = apiTwitterService.getTweetListConfiguration();
+  }
+
+  checkFilterOn() {
+    for (const key in this.tweetListConfiguration) {
+      if (this.tweetListConfiguration.hasOwnProperty(key) && this.tweetListConfiguration[key]) {
+        return true;
+      }
+    }
   }
 
   getTweets() {
     if (this.tweetCalls == 0) {
       this.apiTwitterService.getTimelineTweets(this.tweetCalls, TWEET_LIMIT_PAGE)
-        .subscribe(tweets => { // or search
-          // const tweets = search.statuses in search
+        .subscribe(tweets => {
           this.tweets = tweets;
           this.maxId = tweets[tweets.length - 1].id;
         });
     }
     else if (this.tweetCalls < TWEET_CALLS_LIMIT) {
       this.apiTwitterService.getTimelineTweets(this.tweetCalls, TWEET_LIMIT_PAGE, this.maxId)
-        .subscribe(tweets => { // or search
-          // const tweets = search.statuses in search
-          this.spinner.show();
+        .subscribe(tweets => {
+          this.checkFilterOn() ? "" : this.spinner.show();
           this.maxId = tweets[tweets.length - 1].id;
           tweets.shift();
           this.tweets = this.tweets.concat(tweets);
-          setTimeout(() => {
+          this.checkFilterOn() ? "" : setTimeout(() => {
             this.spinner.hide();
           }, 1000);
         });
